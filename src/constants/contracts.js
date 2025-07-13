febe0915dcddf4c6652bce13c2f77ac94871eb44
Saugat2002 <rockingsaugat777@gmail.com>
2025-07-14 01:10:12 +0545

    changes

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,17 +1,17 @@
 // Contract addresses and configuration
 
 // Deployed package ID from sui client publish
-const PACKAGE_ID = '0xa59e7d128d008793d0c7e3b11bdf09b2731d305ee3d3b03b3fd091c15f3a250c'
+const PACKAGE_ID = '0xc27670350fc823565b2da9b89a52c61e1d27df77088d2de6291f6bd7ac339182'
 
 // Admin wallet address (the wallet that has AdminCap)
 const ADMIN_WALLET_ADDRESS = '0xf446298aa6a47f9b619ec394eb5764c3763a76629aa14c94394663d24733eab4'
 
 // AdminCap created during deployment
-const ADMIN_CAP_ID = '0x793f9413ecec293dfc067b817037657c4df6cb0d1d179a1a426f6fc8152d6ec5'
+const ADMIN_CAP_ID = '0x339f0ce84a93173264409c0f9ba1838b7e8cc1deb1e8dd8c318a0812bf424218'
 
 export const CONTRACTS = {
-  PACKAGE_ID: "0x2482d6f76254ecb6fb2c1bbb844c810ddd7b317ff45eabbe6c6039ac0f2f2713",
-  ADMIN_CAP_ID: "0x0f435128d189e9d3ef16cf611b7b956aaa2464dfba21681ca1c0da5804746912",
+  PACKAGE_ID,
+  ADMIN_CAP_ID
 }
 
 export const NETWORK = {
